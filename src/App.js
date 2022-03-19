28e4e4a6a743515c6321ab37eca2ff676c99b8a0
Vishwas Chandran <apollovishwas@gmail.com>
2022-03-19 13:08:22 -0400

    The pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { Switch, Route } from "react-router-dom";
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Nav from "./Components/Nav";
+import Home from "./Pages/Home";
+import Starred from "./Pages/Starred";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is the home page
-      </Route>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/Starred">
-        This one's starred
-      </Route>
+        <Route exact path="/Starred">
+          <Starred />
+        </Route>
 
-      <Route>This is 404 page. Not Found</Route>
-    </Switch>
+        <Route>This is 404 page. Not Found</Route>
+      </Switch>
+    </div>
   );
 }
 

cc4792e4ac29afd98744ce700b786cca3aa44fe1
Vishwas Chandran <apollovishwas@gmail.com>
2022-03-18 23:48:22 -0400

    added switches and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Switch, Route } from "react-router-dom";
+import React from "react";
+
 function App() {
-  return <div className="App"> hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is the home page
+      </Route>
+
+      <Route exact path="/Starred">
+        This one's starred
+      </Route>
+
+      <Route>This is 404 page. Not Found</Route>
+    </Switch>
+  );
 }
 
 export default App;
